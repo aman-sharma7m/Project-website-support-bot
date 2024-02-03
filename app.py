ede8c3d1ce55721c75fb49038290d65f0a425a8d
aman-sharma7m <aman.7m@outlook.com>
2024-02-03 16:32:06 +0530

    base frontend ready

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 import os 
+import time
 
 
 
 st.set_page_config(page_title='Support chatbot for excelcult')
-st.header('AI assistance for the website')
+st.header('AI assistance for the website 😎😎')
+
+
+#session states
+if 'hug_key' not in st.session_state:
+  st.session_state['hug_key']=''
+if 'pine_key' not in st.session_state:
+  st.session_state['pine_key']=''
 
 
 #side menu
@@ -15,20 +23,45 @@ submit= st.sidebar.button('Submit')
 
 if submit:
   if pine_key!='' and hug_key!='':
-    st.sidebar.success('Got the keys! proceed')
-    #input the query
-    query=st.text_input('Enter the organization name or role looking for:')
+    st.sidebar.success('👍 Keys Success!')
+    st.session_state['hug_key']=hug_key
+    st.session_state['pine_key']=pine_key
+    with st.spinner('Wait for it...'):
+      #scrape the data and get it 
+      st.sidebar.write('🌍 scraping the data!!!! (1 of 4)')
+      #chunk it 
+      st.sidebar.write('🪓 Splitting scraped data!!! (2 of 4)')
+      #get_embeddings
+      time.sleep(5)
+      st.sidebar.write('📃 Generating embeddings (3 of 4)')
+      #create index and push the data-embedding to index
+      st.sidebar.write('🏬 Storing the data embeddings (4 of 4)')
+    #done
+    st.sidebar.success('👉 Proceed to enter the query >>>>>>')
 
-    #input how many links required 
-    nlinks=st.slider('Select the no of rows:',1,10)
-
-    #search button 
-    search_b=st.button('Search')
-
-    if search_b:
-      st.success('Generating the result')
-      st.subheader('Answers:',divider='rainbow')
-      st.write(query)
-      st.write(nlinks)
   else:
-    st.sidebar.error('Please enter the actual keys')
+    st.sidebar.error('Error! Please enter the actual keys')
+
+
+#input the query
+query=st.text_input('Enter the organization name or role looking for:')
+
+#input how many links required 
+nlinks=st.slider('Select the no of rows:',1,10)
+
+#search button 
+search_b=st.button('Search')
+
+if search_b:
+  if st.session_state['pine_key']!='' and st.session_state['hug_key']!='':
+    st.success('Generating the result')
+    st.write(query)
+    st.write(nlinks)
+    st.subheader('Answers:',divider='rainbow')
+    #retreival of documents
+
+    #structured output
+    
+    
+  else:
+    st.error('Invalid keys')
